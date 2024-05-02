75893fcf06244511969bb819d62948cba321b60a
GiaNgan1812 <saomai241202@gmail.com>
2024-05-02 22:50:37 +0700

    search bustime

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -12,51 +12,45 @@ let getSearchpage = async (req, res) => {
 
         console.log(req.query.keyword1);
         console.log(req.query.keyword2);
-        //const result = await db.collection('MOVIES').findOne({ title: req.query.keyword });
-        //const result = await db.collection('MOVIES').findOne({ title: { $regex: req.query.keyword, $options: 'i' } });
-        // const result = await db.collection('Bus').find({
-        //     $or: [
-        //         { Departure: { $regex: req.query.keyword1, $options: 'i' } },
-        //         { Destination: { $regex: req.query.keyword2, $options: 'i' } }
-        //     ]
-        // }).toArray();
 
-        // if (result) {
-        //     res.render('Search.ejs', { bus: result });
-        // } else {
-        //     res.render('Error.ejs', { message: "Chuyến xe này không tồn tại" });
-        // }
-        let query = {};
-
+        let query = [];
         if (req.query.keyword1) {
-            query.Departure = { $regex: req.query.keyword1, $options: 'i' };
+            query.push({ Departure: { $regex: req.query.keyword1, $options: 'i' } });
         }
         if (req.query.keyword2) {
-            query.Destination = { $regex: req.query.keyword2, $options: 'i' };
+            query.push({ Destination: { $regex: req.query.keyword2, $options: 'i' } });
         }
-        if (!req.query.keyword1 && !req.query.keyword2) {
+        if (query.length === 0) {
             return res.render('Error.ejs', { message: "Please enter the keyword!" });
         }
-        const result = await db.collection('Bus').find({ $or: [query] }).toArray();
+
+        const result = await db.collection('Bus').aggregate([
+            { $match: { $or: query } }, // Lọc các bus theo điều kiện tìm kiếm
+            { $unwind: "$ID_bustime" }, // Tách mỗi phần tử trong mảng ID_bustime thành một dòng riêng biệt
+            {
+                $lookup: {
+                    from: "Bus_Time", // Tên của collection để lookup
+                    localField: "ID_bustime", // Trường trong collection Bus chứa id của Bustime
+                    foreignField: "ID", // Trường trong collection Bustime chứa id của mình
+                    as: "bustimeInfo" // Tên của trường để lưu thông tin lookup
+                }
+            }
+        ]).toArray();
+
+
+        console.log(result);
         if (result.length > 0) {
-            // const busIds = result.map(bus => bus.ID_bustime).flat(); // Lấy danh sách id của bustime từ kết quả
-            // const bustimes = await db.collection('Bus_Time').find({ id: { $in: busIds } }).toArray(); // Truy vấn bustime tương ứng
-            // const mergedResults = result.map(bus => {
-            //     const busBustimes = bustimes.filter(bustime => bus.ID_bustime.includes(bustime.ID_buses)); // Lọc bustime tương ứng với từng bus
-            //     return { ...bus, bustimes: busBustimes }; // Kết hợp thông tin của bus và bustime
-            // });
-            // console.log(mergedResults);
             res.render('Search.ejs', { bus: result });
         } else {
             res.render('Error.ejs', { message: "This bus does not exist!" });
         }
-
     }
     catch (err) {
         console.error('Error when searching, please try again!!: ', err);
         res.render('Error.ejs', { message: "Error when searching, please try again!!" });
     }
 }
+
 
 module.exports = {
     getHomepage,
